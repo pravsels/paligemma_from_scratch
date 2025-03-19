96420aa58b295ae4df645d56617cae0be82e93a3
Praveen Selvaraj <praveen.beatle@gmail.com>
2025-03-19 12:12:22 +0000

    gemma config

--- gemma.py
+++ gemma.py
@@ -6,6 +6,38 @@ from torch.nn import CrossEntropyLoss
 import math 
 from siglip import SiglipVisionConfig, SiglipVisionTransformer
 
+class GemmaConfig():
+    def __init__(
+        self,
+        vocab_size,
+        hidden_size,
+        intermediate_size,
+        num_hidden_layers,
+        num_attention_heads,
+        num_key_value_heads,
+        head_dim=256,
+        max_position_embeddings=8192,
+        rms_norm_eps=1e-6,
+        rope_theta=10000.0,
+        attention_bias=False,
+        attention_dropout=0.0,
+        pad_token_id=None,
+        **kwargs,
+    ):
+        super().__init__() 
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_key_value_heads = num_key_value_heads
+        self.head_dim = head_dim
+        self.max_position_embeddings = max_position_embeddings
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.pad_token_id = pad_token_id
 
 class PaliGemmaConfig():
     def __init__(
